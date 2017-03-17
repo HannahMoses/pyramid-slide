e28401bccf8872e9a8db813163a140f42f6dd8ed
HannahMoses <hannahmoses612@gmail.com>
2017-03-17 04:04:34 -0500

    brick symbol and height sliding done

---
+++ slide.js
@@ -0,0 +1,10 @@
+// **LC101 Unit3 slide.js to be used with pyramidslide.html
+
+function slideFunction(value){
+    document.getElementById("slider").innerHTML;
+    document.getElementById("value").innerHTML = value;
+}
+
+function printPyramid(height) {
+
+    }
